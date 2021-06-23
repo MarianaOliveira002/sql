530a9742d2483cd121e1a57789c30a44758d5005
MariOliveira <79949514+MarianaOliveira002@users.noreply.github.com>
2021-06-22 22:48:39 -0300

    Add files via upload

---
+++ aula 06 apagando tabela.sql
@@ -0,0 +1,12 @@
+create table if not exists teste (
+id int,
+nome varchar(20),
+idade int
+);
+
+insert into teste value 
+('1', 'mariana', '12'),
+('2', 'maria', '2');
+
+select * from teste;
+drop table if exists teste; 
